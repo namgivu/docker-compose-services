9e11524776d95826625456d99b2b5cdc05fe19f5
namgivu <nam.gi.vu@gigacover.com>
2019-09-22 13:05:38 +0700

    circleci-xenial

---
+++ circleci-xenial/up.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME="$s"  # get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
+
+CONTAINER_NAME='nn_xenial'
+
+# run the container
+docker-compose -f "$SCRIPT_HOME/docker-compose.yml" up -d --force-recreate  # ref. https://forums.docker.com/t/named-volume-with-postgresql-doesnt-keep-databases-data/7434/2
+
+# aftermath note
+echo "
+# after container run, we can use 'psql' via
+docker exec -it $CONTAINER_NAME bash
+"
